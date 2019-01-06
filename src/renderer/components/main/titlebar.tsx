4710b0e8e3dfe8565df0d9c04744389bf2af5f50
Fabio Spampinato <spampinabio@gmail.com>
2019-01-06 18:00:57 +0100

    Hiding close button on non-macOS systems

--- src/renderer/components/main/titlebar.tsx
+++ src/renderer/components/main/titlebar.tsx
@@ -3,6 +3,7 @@
 
 import * as React from 'react';
 import {ipcRenderer as ipc} from 'electron';
+import * as is from 'electron-is';
 
 /* HELPERS */
 
@@ -12,11 +13,13 @@ const windowClose = () => ipc.send ( 'window-close' );
 
 const Titlebar = ({ title, titles, onChange }) => (
   <div id="titlebar">
-    <div id="titlebar-close" onClick={windowClose}>
-      <svg x="0px" y="0px" viewBox="0 0 6.4 6.4">
-        <polygon fill="#4d0000" points="6.4,0.8 5.6,0 3.2,2.4 0.8,0 0,0.8 2.4,3.2 0,5.6 0.8,6.4 3.2,4 5.6,6.4 6.4,5.6 4,3.2"></polygon>
-      </svg>
-    </div>
+    {!is.macOS () ? null : (
+      <div id="titlebar-close" onClick={windowClose}>
+        <svg x="0px" y="0px" viewBox="0 0 6.4 6.4">
+          <polygon fill="#4d0000" points="6.4,0.8 5.6,0 3.2,2.4 0.8,0 0,0.8 2.4,3.2 0,5.6 0.8,6.4 3.2,4 5.6,6.4 6.4,5.6 4,3.2"></polygon>
+        </svg>
+      </div>
+    )}
     <div id="titlebar-title">
       <span>{title}</span>
       <select id="titlebar-select" value={title} onChange={onChange}>
